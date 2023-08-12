5a78f87c2f458e422581ad81cbbe21cc296e0293
Amritendu De <amritendu_de@yahoo.com>
2023-08-12 10:13:34 +0530

    Prime

--- src/test/java/com/scaler/module01/isprime/IsPrimeTest.java
+++ src/test/java/com/scaler/module01/isprime/IsPrimeTest.java
@@ -1,6 +1,5 @@
-package com.isprime.module01.isprime;
+package com.scaler.module01.isprime;
 
-import com.scaler.module01.isprime.IsPrime;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
