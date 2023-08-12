6ac62e2bc546762017a30ddf0e4aa0d799c6d193
Amritendu De <amritendu_de@yahoo.com>
2023-08-12 10:48:48 +0530

    Comments on the problem

--- src/main/java/com/scaler/module01/arrayrotation/ArrayRotation.java
+++ src/main/java/com/scaler/module01/arrayrotation/ArrayRotation.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 /*
 Given an integer array A of size N and an integer B,
 you have to return the same array after rotating it B times towards the right.
+Rotate towards the right 2 times - [1, 2, 3, 4] => [4, 1, 2, 3] => [3, 4, 1, 2]
  */
 public class ArrayRotation {
     public ArrayList<Integer> solve(ArrayList<Integer> A, int B) {
